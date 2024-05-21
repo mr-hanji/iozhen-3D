667469eb816aa4977e5bcda3240a693ae3260fe9
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2024-05-21 15:39:07 +0330

    add site to the monitor

--- src/Models/Couch.jsx
+++ src/Models/Couch.jsx
@@ -3,9 +3,25 @@ import { useGLTF , Clone } from '@react-three/drei'
 
 export default function Couch({officeLength, officeWidth }) {
     const couch = useGLTF('/objects/Couch.glb')
+
+    const eventHandler = (event) => {
+      // event.stopPropagation baray vaghti ke ye shei dige joloy shei aslie
+      couch.scene.traverse((child) => {
+        if(child.isMesh) {
+          child.material.color.set("#ff0000");
+        }
+      })
+    }
+
+
   return (
     <>
-        <primitive object={couch.scene}  scale={2} position={[officeWidth / 2 , 0, officeLength / 2]} rotation-y={Math.PI} />
+        <primitive object={couch.scene}  scale={2} position={[officeWidth / 2 , 0, officeLength / 2]} rotation-y={Math.PI} onClick={eventHandler}/>  {/* onContextMenu is for right click */} {/* onDoubleClick */} 
+        {/* onPointerUp and onPointerDown */}
+        {/* onPointerOver and onPointerEnter */} {/* onPointerOut and onPointerLeave */} {/* onPointerMove */} {/* onPointerMissed */} 
+        {/* useCurser is for change curser  */}
+        
+        
     </>
   )
 }
--- src/Models/Monitors.jsx
+++ src/Models/Monitors.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
-import { useGLTF , Clone } from '@react-three/drei'
+import { useGLTF , Clone , Html } from '@react-three/drei'
+import { useControls } from 'leva'
 
 export default function Monitors({tableHeight}) {
     const monitor = useGLTF('/objects/Monitor.glb')
+
+    const {lightPosition} = useControls('light' , {
+      lightPosition : {value : [0 , 0 , 0]}
+  })
+
   return (
     <>
-        <Clone object={monitor.scene} scale={2} position={[8 , tableHeight , -8]}   rotation-y={Math.PI / 2}/>
+        <Clone object={monitor.scene} scale={2} position={[8 , tableHeight , -8]}   rotation-y={Math.PI / 2}>
+          <rectAreaLight width={2.5} height={1.65} intensity={20} color={"#0e1c63"} rotation={[-0.1 , Math.PI , 0]} position={lightPosition}/>
+        <Html
+          
+          rotation-y={3.14 / 2}
+          transform
+          wrapperClass="htmlScreen"
+          distanceFactor={0.42}
+          position={[0.05, 0.585, -0.01]} // Adjusted the position to align with the TV screen
+        >
+          <div className='iframe-container'>
+            <iframe src="https://iozhen.com" />
+          </div>
+          </Html>
+        </Clone>
         <Clone object={monitor.scene} scale={2} position={[-7 , tableHeight , -8]} rotation-y={Math.PI}/>
         <Clone object={monitor.scene} scale={2} position={[-7 , tableHeight , 0]}  rotation-y={Math.PI}/>
         <Clone object={monitor.scene} scale={2} position={[-7 , tableHeight , 8]}  rotation-y={Math.PI}/>
